1b0f7f3d4538f54846aee367fd7a693668916799
oleksandr-chornovol <48795176+oleksandr-chornovol@users.noreply.github.com>
2021-11-10 02:08:42 +0200

    Create Task3.php

---
+++ OOP/Task3.php
@@ -0,0 +1,11 @@
+<?php 
+
+class CurrentUSPresident
+{
+  public const name = 'Barack Obama';
+  
+  public static function greet($name) : string
+  {
+    return "Hello $name, my name is Barack Obama, nice to meet you!";
+  }
+}
